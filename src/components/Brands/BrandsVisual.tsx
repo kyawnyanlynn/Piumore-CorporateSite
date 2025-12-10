40bfbe3fedd3e126ea2110a36775c6aec943b8f9
Liu Peiyu <24aw0142@jec.ac.jp>
2025-12-10 09:30:18 +0900

    update：スマホバージョンのファストビュの写真の修正

--- src/components/Brands/BrandsVisual.tsx
+++ src/components/Brands/BrandsVisual.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const BrandsVisual = ({ imge }: { imge: string }) => {
       return (
         <>
-            <div className="bg-gray w-screen h-screen">
+            <div className="w-full object-cover translate-y-0 sm:translate-y-10">
                 <img src={imge} alt="" />
             </div>
         </>
