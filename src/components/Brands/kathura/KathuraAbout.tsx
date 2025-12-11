5657b0e5c83c22535cc877acad0ba1cdcc8dfe95
Liu Peiyu <24aw0142@jec.ac.jp>
2025-12-11 12:30:14 +0900

    add:kathuraのコンセプトの文を追加しました。

---
+++ src/components/Brands/kathura/KathuraAbout.tsx
@@ -0,0 +1,23 @@
+
+
+
+
+const KathuraAbout = () => {
+    return (
+          <>
+            <div className="flex flex-col items-center mt-10 sm:mt-30 leading-relaxed tracking-wide">
+                <h2 className="text-center text-2xl sm:text-5xl pb-8 pt-6 text-[#B42423] ">コンセプト</h2>
+                <div className="text-[18px] pb-7">
+                     <p className="text-left px-[5%] sm:px-0 text-lg ">
+                        歌舞伎の演目にも登場する伝統味噌を使い江戸時代のレシピを再現した江戸前すき焼きが名物のお店。
+                    </p>
+                    <p className="text-left px-[5%] sm:px-0  text-base sm:text-lg">
+                      接待や会合、宴会など様々なシーンでご利用頂いているお店です。
+                    </p>
+                </div>
+              </div>
+          </>
+    )
+}
+
+export default KathuraAbout;
