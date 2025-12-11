585d73fe33a5b0ffe61bb8d7f2b8eb41037924b7
Liu Peiyu <24aw0142@jec.ac.jp>
2025-12-11 14:32:32 +0900

    add：会社概要のコンテンツを追加しました。

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import BrandsIndex from "./components/Brands/Lauro/BrandsIndex";
 import "./App.css";
 import RecruitIntex from "./components/RecruitmentInformation/RecruitIntex";
 import KathuraIndex from "./components/Brands/kathura/KathuraIndex";
+import CompanyIndex from "./components/CompanyProfile/CompanyIndex";
 
 function App() {
   return (
     <>
       {/* <BrandsIndex /> */}
       {/* <RecruitIntex /> */}
-      <KathuraIndex />
+      {/* <KathuraIndex /> */}
+      <CompanyIndex />
     </>
   );
 }
---
+++ src/components/CompanyProfile/CompanyIndex.tsx
@@ -0,0 +1,10 @@
+import CompanyAbout from "./CompanyAbout";
+
+const CompanyIndex = () => {
+    return (
+        <>
+            <CompanyAbout />
+        </>
+    )
+};
+export default CompanyIndex;
